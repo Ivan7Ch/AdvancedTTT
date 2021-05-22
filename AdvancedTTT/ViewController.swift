141982dc4a9c9160df23440a748351f22539f4f1
Ivan Chernetskyi <Ivan.Chernetskyi@medhost.com>
2021-05-22 08:44:06 +0300

    Initial Commit

---
+++ AdvancedTTT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvancedTTT
+//
+//  Created by Ivan Chernetskyi on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
