48d5cae594e90765cac519cce711a4a24c7e115f
Ivan Chernetskyi <Ivan.Chernetskyi@medhost.com>
2021-05-30 21:55:36 +0300

    fixed end game logic

--- AdvancedTTT/ViewModel.swift
+++ AdvancedTTT/ViewModel.swift
@@ -12,51 +12,55 @@ class ViewModel {
     
     let vc: ViewController
     
-    private var matrixDoble = Array(repeating: Array(repeating: Item(power: 0, side: .unknown), count: 3), count: 3)
+    private var matrix = [[Item]]()
+    private let sideLenght = 3
+    
     
     init(vc: ViewController) {
         self.vc = vc
-        
-        initDoubleMatrix()
     }
     
-    private func initDoubleMatrix() {
-        
-        let doubleMatrixLength: Int = Int(sqrt(Double(vc.mainSource.count)))
-        
-        for i in 0..<doubleMatrixLength {
-            for j in 0..<doubleMatrixLength {
-                matrixDoble[i][j].power = vc.mainSource[i * doubleMatrixLength + j].power
-                matrixDoble[i][j].side = vc.mainSource[i * doubleMatrixLength + j].side
+    private func setupMatrix() {
+        matrix = [[Item]]()
+        for i in 0..<sideLenght {
+            var arr = [Item]()
+            for j in 0..<sideLenght {
+                let item = vc.mainSource[i * sideLenght + j]
+                arr.append(item)
             }
+            matrix.append(arr)
         }
     }
     
     func check() {
-        initDoubleMatrix()
-        
-        for j in 0..<3 {
-            if matrixDoble[j][0].side == .unknown { continue }
-            if matrixDoble[j][0].side == matrixDoble[j][1].side, matrixDoble[j][1].side == matrixDoble[j][2].side {
-                vc.showWinAlert()
-                return
-            }
-        }
-        
-        for j in 0..<3 {
-            if matrixDoble[0][j].side == .unknown { continue }
-            if matrixDoble[0][j].side == matrixDoble[1][j].side, matrixDoble[1][j].side == matrixDoble[2][j].side {
-                vc.showWinAlert()
-                return
-            }
-        }
-        
-        if matrixDoble[0][0].side != .unknown, matrixDoble[0][0].side == matrixDoble[1][1].side, matrixDoble[1][1].side == matrixDoble[2][2].side {
+        setupMatrix()
+        if vc.redSource.filter({ $0.side == .unknown }).count == 6 || vc.blueSource.filter({ $0.side == .unknown }).count == 6 {
             vc.showWinAlert()
             return
         }
         
-        if matrixDoble[0][2].side != .unknown, matrixDoble[0][2].side == matrixDoble[1][1].side, matrixDoble[1][1].side == matrixDoble[2][0].side {
+        for j in 0..<3 {
+            if matrix[j][0].side == .unknown { continue }
+            if matrix[j][0].side == matrix[j][1].side, matrix[j][1].side == matrix[j][2].side {
+                vc.showWinAlert()
+                return
+            }
+        }
+        
+        for j in 0..<3 {
+            if matrix[0][j].side == .unknown { continue }
+            if matrix[0][j].side == matrix[1][j].side, matrix[1][j].side == matrix[2][j].side {
+                vc.showWinAlert()
+                return
+            }
+        }
+        
+        if matrix[0][0].side != .unknown, matrix[0][0].side == matrix[1][1].side, matrix[1][1].side == matrix[2][2].side {
+            vc.showWinAlert()
+            return
+        }
+        
+        if matrix[0][2].side != .unknown, matrix[0][2].side == matrix[1][1].side, matrix[1][1].side == matrix[2][0].side {
             vc.showWinAlert()
             return
         }
